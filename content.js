c30adeaf91345c55b105eb957f800f21d42c54c1
Jean-Baptiste Ronssin <jean-baptiste.ronssin@lifen.fr>
2021-07-20 16:25:23 +0200

    initial

--- content.js
+++ content.js
@@ -1,6 +1,4 @@
 var links = document.getElementsByTagName('a');
-var len = links.length;
-for(var i=0; i<len; i++)
-{
-   links[i].target = "_blank";
+for (var i=0, len=links.length; i < len; i++) {
+  links[i].target = '_blank';
 }
