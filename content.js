616ef107cf70962b13e241dd4ace7d394dffa379
clemlifen <clement.boschet@lifen.fr>
2021-07-20 16:51:12 +0200

    Add Listener on click events

--- content.js
+++ content.js
@@ -1,4 +1,13 @@
-var links = document.getElementsByTagName('a');
-for (var i=0; i < links.length; i++) {
-  links[i].target = '_blank';
+function search_links() {
+  let links = document.getElementsByTagName('a');
+  console.log(links);
+  for (var i=0; i < links.length; i++) {
+    links[i].target = '_blank';
+  }
 }
+
+search_links();
+
+document.getElementById('jira').addEventListener("click", function() {
+  search_links();
+})
