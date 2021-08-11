b87efb2c6348026302f810d34a53c1492d318150
clemlifen <clement.boschet@lifen.fr>
2021-08-11 08:48:27 +0200

    Apply only on external links

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.getElementById('jira').addEventListener('click', (event) => {
-  if(event.target.href) {
+  if(event.target.href && event.target.href.startsWith('http')) {
     event.preventDefault();
     event.stopPropagation();
     window.open(event.target.href, '_blank')
